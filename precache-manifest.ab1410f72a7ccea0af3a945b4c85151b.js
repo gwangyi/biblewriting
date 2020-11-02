c33eaa4ea76f652b98aad60068653fc07bd52de6
Sungkwang Lee <gwangyi.kr@gmail.com>
2020-11-02 23:06:19 +0100

    deploy

---
+++ precache-manifest.ab1410f72a7ccea0af3a945b4c85151b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c002c79f9bdbdfe7784f",
+    "url": "/biblewriting/css/app.1e593995.css"
+  },
+  {
+    "revision": "d7a85301a3cfbe3515e4",
+    "url": "/biblewriting/css/chunk-vendors.3ef6ec65.css"
+  },
+  {
+    "revision": "199b774b246d8cbdf690bc818fca7f2a",
+    "url": "/biblewriting/img/logo.199b774b.png"
+  },
+  {
+    "revision": "58303b85a2fa0053f1641f5d820738ad",
+    "url": "/biblewriting/index.html"
+  },
+  {
+    "revision": "c002c79f9bdbdfe7784f",
+    "url": "/biblewriting/js/app.6017c166.js"
+  },
+  {
+    "revision": "d7a85301a3cfbe3515e4",
+    "url": "/biblewriting/js/chunk-vendors.9e9466e4.js"
+  },
+  {
+    "revision": "f1298c1b0e3eeb271bb9eba9e91f601c",
+    "url": "/biblewriting/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/biblewriting/robots.txt"
+  }
+]);
